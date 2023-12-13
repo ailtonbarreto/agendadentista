1995798a46f88ebff242e3e6c495a6184be59fca
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-12 22:27:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import openpyxl as xl
 #------------------------------------------------------------------------------
-# Configuração da página
-st.set_page_config(layout='wide',page_icon='🦷',page_title="Agenda",initial_sidebar_state='collapsed')
-st.title('📘 🦷 Agenda - Consultório Dr. Fulano')
+# Configuração da páginas
+st.set_page_config(layout='wide',page_icon='🦷',page_title="Agenda",initial_sidebar_state='collapsed',menu_items=None)
+st.title('📘 🦷 Agenda - Consultório Dr. Fulano',anchor=False)
 st.markdown('--------')
 
 #------------------------------------------------------------------------------
@@ -23,9 +23,9 @@ col1, col2 = st.columns(2)
 
 #------------------------------------------------------------------------------
 # Barra Lateral
-st.sidebar.image('paciente.png',width=200)
+st.sidebar.image('paciente.png',width=200,caption="",use_column_width=True)
 st.sidebar.markdown('---')
-st.sidebar.title("Cadastrar Paciente")
+st.sidebar.title("Cadastrar Paciente",anchor=False)
 
 nome = st.sidebar.text_input("Nome","")
 telefone = st.sidebar.text_input("Telefone")
@@ -33,26 +33,31 @@ telefone = st.sidebar.text_input("Telefone")
 #------------------------------------------------------------------------------
 # Barra lateral
 with col1:
+    st.subheader('Agendar',anchor=False)
     entrada_data = st.date_input("Data", 'today', format="DD/MM/YYYY")
     entrada_paciente = st.selectbox("Paciente",dfpaciente['Pacientes'].unique()) 
     entrada_procedimento = st.selectbox("Procedimento",df['Procedimento'].unique())
     entrada_hora = st.time_input("Horário")
+with col2:
+    st.subheader('Agenda do dia',anchor=False)
+    dff= df.query("Data == @entrada_data")
+    st.table(dff)
     
 #------------------------------------------------------------------------------
 # Adicionar na agenda
-if st.button("Agendar"):
+with col1:
+    if st.button("Agendar"):
     # Abra o arquivo do Excel
-    planilha = xl.load_workbook("Agenda.xlsx")
-    planilha = planilha.active
+        planilha = xl.load_workbook("Agenda.xlsx")
+        planilha = planilha.active
 
-    nova_linha = [entrada_data, entrada_paciente,entrada_procedimento, entrada_hora]
+        nova_linha = [entrada_data, entrada_paciente,entrada_procedimento, entrada_hora]
 
-    planilha.append(nova_linha)
+        planilha.append(nova_linha)
 
-    planilha.parent.save("Agenda.xlsx")
-   
-    st.success("Agendamento salvo!")
-    
+        planilha.parent.save("Agenda.xlsx")
+        
+        st.success("Agendamento salvo!")
 
 #------------------------------------------------------------------------------
 if st.sidebar.button("Salvar"):
@@ -67,13 +72,18 @@ if st.sidebar.button("Salvar"):
     planilha.parent.save("Pacientes.xlsx")
     
     st.sidebar.success("Cadastro salvo!")
-    
 #------------------------------------------------------------------------------
 
 st.markdown('--------')
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 #------------------------------------------------------------------------------
-# Exibir tabela
-dff= df.query("Data == @entrada_data")
-st.table(dff)
 
