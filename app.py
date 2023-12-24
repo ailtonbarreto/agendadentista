2049b5c8b89dcd6926757462806cfee69affb6f2
Ailton Barreto <143955093+ailtonbarreto@users.noreply.github.com>
2023-12-24 12:31:51 -0300

    a

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ Agenda = pd.DataFrame({"Data":[dt.date.today(),dt.date.today(),dt.date.today(),d
                 hora-dt.timedelta(minutes=90),
                 hora-dt.timedelta(minutes=180),
                 hora-dt.timedelta(minutes=180)]})
-Agenda["Hora"] = Agenda["Hora"].strftime('%H:%M')
+Agenda['Hora'] = pd.to_datetime(Agenda['Hora'], format='%H:%M')
 
 tabela = pd.read_excel('Agenda.xlsx')
 df = pd.DataFrame(Agenda)
